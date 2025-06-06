eb03db8c263ddbe93992350ef70547001fda0f10
Yaren Bulut <your.email@example.com>
2025-06-06 23:40:55 +0300

    Simplify deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,34 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3001,
-    host: true
-  },
-  base: '/',
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "bootstrap/scss/bootstrap";`
-      }
-    }
-  },
-  assetsInclude: ['**/*.PNG', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg', '**/*.woff', '**/*.woff2', '**/*.eot', '**/*.ttf'],
-  publicDir: 'public',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        main: 'index.html'
-      }
-    },
-    chunkSizeWarningLimit: 1000
+    outDir: 'dist'
   }
 })
